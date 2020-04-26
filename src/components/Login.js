70a60924be75da2699768938478d52a8a0961993
Mario-Galindo <mgalindo9410@gmail.com>
2020-04-26 17:25:26 -0600

    Add render view to Login Component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,1 +1,15 @@
 import React from 'react';
+
+class Login extends React.Component {
+    render() {
+        return (
+            <header className="App-header">
+                <p>
+                    Iniciar Sesion
+                </p>
+            </header>
+        )
+    }
+}
+
+export default Login;
