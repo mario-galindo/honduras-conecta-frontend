aab3d46f8d39cbb0bf7ece349e7be5242b4781eb
Mario-Galindo <mgalindo9410@gmail.com>
2020-04-26 20:56:30 -0600

    change format

--- src/components/main.js
+++ src/components/main.js
@@ -1,31 +1,31 @@
 import React from 'react';
 
 const cardStyle = {
-    color:"#000",
+    color: "#000",
     margin: {
         // jss-expand gives more readable syntax
-        
+
         bottom: 50, // jss-default-unit makes this 5px
-      },
-      padding:{
+    },
+    padding: {
         bottom: 50,
-        top:40
+        top: 40
 
-      }
-  };
+    }
+};
 
-class Main extends React.Component{
-    render(){
-        return(
+class Main extends React.Component {
+    render() {
+        return (
             <div className="col-md-4">
                 <div className="card" style={cardStyle.margin}>
                     <div className="card-body">
-        <h5 className="card-title">{this.props.title}</h5>
+                        <h5 className="card-title">{this.props.title}</h5>
                         <p className="card-text">Unete a la causa y se parte del voluntariado.</p>
-        <a href="#" className="btn btn-primary">{this.props.boton}</a>
+                        <a href="#" className="btn btn-primary">{this.props.boton}</a>
                     </div>
                 </div>
-            </div>  
+            </div>
         );
     }
 }
